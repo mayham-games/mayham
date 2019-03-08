493f076f687519d2c95860a8cf410a0945a9f171
DJ Simpson <dougjr.simpson@gmail.com>
2019-03-08 16:32:35 -0700

    Main goal logic (#1)
    * Main goal logic
    
    I add the main goal logic with a timer that increments player's scores inside the goal area and decrements the time remaining every second.
    
    * Revision changes
    
    I changed the check on_Goal_entered/exited from type not "StaticBody2D" to "KinematicBody2D";
    I also added comments to show what lines in Player.gd I changed
    
    * Fixed the Goal bug
    
    Fixed the goal lists so it set the next of new tail to null in pop_back and set the previous of new head to null in pop_front.
    
    * Delete KinematicBody2D.gd
    
    Are you happy Carter?

--- mayham-game/Goal.gd
+++ mayham-game/Goal.gd
@@ -4,10 +4,62 @@ extends Area2D
 # var a = 2
 # var b = "textvar"
 
+var players_in_goal = null
+var players_not_in_goal = null
+
+func init(players):
+	var player_list = ResourceLoader.load("res://PlayerLinkedList.gd")
+	players_in_goal = player_list.new()
+	players_not_in_goal = player_list.new()
+	for player in players:
+		players_not_in_goal.push_back(player)
+	print("init:")
+	print_lists()
+
+func print_lists():
+	print("players_in_goal:")
+	players_in_goal.print_list()
+	print("players_not_in_goal:")
+	players_not_in_goal.print_list()
+
 func _ready():
+	connect("body_entered", self, "_on_Goal_entered")
+	connect("body_exited", self, "_on_Goal_exited")
+
 	# Called when the node is added to the scene for the first time.
 	# Initialization here
 	pass
+
+func _on_Goal_entered(body):
+	if body.is_class("KinematicBody2D"):
+		var player = players_not_in_goal.pop_by_number(body.get_number())
+		players_in_goal.push_back(player)
+		print("_on_Goal_entered:")
+		print_lists()
+
+func _on_Goal_exited(body):
+	if body.is_class("KinematicBody2D"):
+		var player = players_in_goal.pop_by_number(body.get_number())
+		players_not_in_goal.push_back(player)
+		print("_on_Goal_exited:")
+		print_lists()
+
+func _on_Goal_moved():
+	players_not_in_goal.push_all(players_in_goal.pop_all())
+	var intersecting_players = get_overlapping_bodies()
+	for player in intersecting_players:
+		if player.is_class("KinematicBody2D"):
+			players_in_goal.push(players_not_in_goal.pop_by_number(player.get_number()))
+
+func award_points():
+	players_in_goal.add_point_to_all()
+
+func collect_players():
+	players_not_in_goal.push_all(players_in_goal.pop_all())
+	return players_not_in_goal.pop_all_ordered_by_highest_score()
+
+func has_winner(winning_score):
+	return players_in_goal.has_winner(winning_score) or players_not_in_goal.has_winner(winning_score)
 
 #func _process(delta):
 #	# Called every frame. Delta is time since last frame.
---
+++ mayham-game/PlayerLinkedList.gd
@@ -0,0 +1,61 @@
+extends 'res://LinkedList.gd'
+
+func pop_all():
+	var temp_list = get_script()
+	while _len > 0:
+		temp_list.push_back(pop_front())
+	return temp_list
+
+func push_all(list):
+	while list.size() > 0:
+		push_back(list.pop_front())
+
+func pop(node):
+	if _head == node:
+		return pop_front()
+	if _tail == node:
+		return pop_back()
+	else:
+		node.unlink()
+		return node.data
+
+func pop_by_number(number):
+	if _len == 0:
+		return null
+	else:
+		var temp = _head
+		while temp != null:
+			if temp.data.get_number() == number:
+				return pop(temp)
+			temp = temp.next
+		return null
+
+func add_point_to_all():
+	var temp = _head
+	while temp != null:
+		temp.data.increment_score_by(1)
+		temp = temp.next
+
+func has_winner(winning_score):
+	if _len == 0:
+		return false
+	var temp = _head
+	while temp != null:
+		if temp.data.get_score() >= winning_score:
+			return true
+		temp = temp.next
+	return false
+
+func pop_all_ordered_by_highest_score(): #clears list as well
+	var ordered = get_script()
+	while _len > 0:
+		var temp = _head
+		var leader = temp
+		var high_score = 0
+		while temp != null:
+			if temp.data.get_score() > high_score:
+				high_score = temp.data.get_score()
+				leader = temp
+			temp = temp.next
+		ordered.push_back(pop(leader))
+	return ordered
