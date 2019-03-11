ae94c09c298bcc6c5a2a9fe165effd52da079ca6
mike0256 <michael.richards256@gmail.com>
2019-03-11 15:39:14 -0600

    Updating player controller based on merge
    Changed the variable player_num to be number after merge

--- mayham-game/PlayerController.gd
+++ mayham-game/PlayerController.gd
@@ -22,23 +22,23 @@ func _ready():
 	print("PC ready ", number)
 	
 func _input(event):
-	if event.is_action_pressed("p" + str(player_num) + "_start"):
+	if event.is_action_pressed("p" + str(number) + "_start"):
 		emit_signal("action_start")
-	if event.is_action_pressed("p" + str(player_num) + "_jump"):
+	if event.is_action_pressed("p" + str(number) + "_jump"):
 		emit_signal("action_jump")
-	if event.is_action_pressed("p" + str(player_num) + "_dash"):
+	if event.is_action_pressed("p" + str(number) + "_dash"):
 		emit_signal("action_dash")
-	if event.is_action_pressed("p" + str(player_num) + "_left"):
+	if event.is_action_pressed("p" + str(number) + "_left"):
 		emit_signal("action_left")
-	elif event.is_action_pressed("p" + str(player_num) + "_right"):
+	elif event.is_action_pressed("p" + str(number) + "_right"):
 		emit_signal("action_right")
-	if event.is_action_released("p" + str(player_num) + "_right") or event.is_action_released("p" + str(player_num) + "_left"):
+	if event.is_action_released("p" + str(number) + "_right") or event.is_action_released("p" + str(number) + "_left"):
 		emit_signal("action_stop")
 
 func _process(delta):
-	if Input.is_action_pressed("p" + str(player_num) + "_left"):
+	if Input.is_action_pressed("p" + str(number) + "_left"):
 		emit_signal("action_left")
-	elif Input.is_action_pressed("p" + str(player_num) + "_right"):
+	elif Input.is_action_pressed("p" + str(number) + "_right"):
 		emit_signal("action_right")
 
 func get_player():
