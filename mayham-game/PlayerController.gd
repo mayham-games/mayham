3ee0890e5069464a93eb97d040aa9be05927aed9
mike0256 <michael.richards256@byu.edu>
2019-03-12 18:49:05 -0600

    Multiple controllers (#3)
    * Added input maps for multiple players
    
    Added new input maps for multiple players, updated the player controller script to only listen for the input from its specific player controller
    
    * Updating player controller based on merge
    
    Changed the variable player_num to be number after merge

--- mayham-game/PlayerController.gd
+++ mayham-game/PlayerController.gd
@@ -22,23 +22,23 @@ func _ready():
 	print("PC ready ", number)
 	
 func _input(event):
-	if event.is_action_pressed("ui_start"):
+	if event.is_action_pressed("p" + str(number) + "_start"):
 		emit_signal("action_start")
-	if event.is_action_pressed("ui_jump"):
+	if event.is_action_pressed("p" + str(number) + "_jump"):
 		emit_signal("action_jump")
-	if event.is_action_pressed("ui_dash"):
+	if event.is_action_pressed("p" + str(number) + "_dash"):
 		emit_signal("action_dash")
-	if event.is_action_pressed("ui_left"):
+	if event.is_action_pressed("p" + str(number) + "_left"):
 		emit_signal("action_left")
-	elif event.is_action_pressed("ui_right"):
+	elif event.is_action_pressed("p" + str(number) + "_right"):
 		emit_signal("action_right")
-	if event.is_action_released("ui_right") or event.is_action_released("ui_left"):
+	if event.is_action_released("p" + str(number) + "_right") or event.is_action_released("p" + str(number) + "_left"):
 		emit_signal("action_stop")
 
 func _process(delta):
-	if Input.is_action_pressed("ui_left"):
+	if Input.is_action_pressed("p" + str(number) + "_left"):
 		emit_signal("action_left")
-	elif Input.is_action_pressed("ui_right"):
+	elif Input.is_action_pressed("p" + str(number) + "_right"):
 		emit_signal("action_right")
 
 func get_player():
