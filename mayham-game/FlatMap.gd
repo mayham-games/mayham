473d25590ee1e2c7669efdef89d73dd9f7fa61bb
SponJ2000 <43590538+SponJ2000@users.noreply.github.com>
2019-03-14 09:26:23 -0600

    Add files via upload
    1) added run animation to player
    2) added hit functions (and a custom state and timer) to enable knockback and hitstun (with variable power and direction)
    3) added custom fonts to the player label and match timer

--- mayham-game/FlatMap.gd
+++ mayham-game/FlatMap.gd
@@ -1,12 +1,15 @@
 extends Node
 
 var goal = null
+onready var timelabel = $MatchTime		# Jordan
 
 func init(players):
 	goal = $Goal
 	goal.init(players)
 
 func _ready():
+	timelabel.add_color_override("font_color",Color(0,0,0,1))			# Jordan
+	timelabel.add_color_override("font_color_shadow",Color(1,1,1,1))	# was here
 	pass
 
 func award_points():
