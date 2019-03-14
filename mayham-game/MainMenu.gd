278f82693cf2066e36bfc43d3f46f271221c6f47
Carter McBride <junk@carter.works>
2019-03-14 17:39:25 -0600

    Start game from main menu

---
+++ mayham-game/MainMenu.gd
@@ -0,0 +1,21 @@
+extends MarginContainer
+
+# class member variables go here, for example:
+# var a = 2
+# var b = "textvar"
+
+func _ready():
+	# Called when the node is added to the scene for the first time.
+	# Initialization here
+	pass
+
+#func _process(delta):
+#	# Called every frame. Delta is time since last frame.
+#	# Update game logic here.
+#	pass
+
+func _start_match():
+	get_tree().change_scene("res://Match.tscn")
+
+func _on_StartGameButton_pressed():
+	_start_match()
