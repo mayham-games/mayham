2a6cf2710082342099e80a6a6975cc47625b1a87
Carter McBride <junk@carter.works>
2019-03-14 18:01:14 -0600

    Add end game skeleton

--- mayham-game/Match.gd
+++ mayham-game/Match.gd
@@ -13,12 +13,31 @@ onready var goal_interval = 20
 
 var _timer = null
 var _map = null
+var _players = []
 
 func _ready():
 	# Add the players
 	var game = ResourceLoader.load("res://FlatMap.tscn")
 	_map = game.instance()
 	add_child(_map)
+	_players = _init_players()
+	_map.init(_players)
+	_init_timer()
+
+
+func _on_Timer_timeout():
+	_map.award_points()
+	time_remaining -= 1
+	_map.update_timer(time_remaining)
+
+
+func _process(delta):
+	# Called every frame. Delta is time since last frame.
+	# Update game logic here.
+	if _map.has_winner(winning_score) or time_remaining <= 0:
+		end_game()
+
+func _init_players():
 	var playerController = ResourceLoader.load("res://PlayerController.tscn")
 	var players = []
 	for i in range(num_players):
@@ -26,9 +45,9 @@ func _ready():
 		playerControl.init(i+1)
 		add_child(playerControl)
 		players.append(playerControl.get_player())
-	_map.init(players)
+	return players
 	
-	# set the time limit
+func _init_timer():
 	time_remaining = time_limit
 	
 	_timer = Timer.new()
@@ -38,18 +57,27 @@ func _ready():
 	_timer.set_one_shot(false) # Make sure it loops
 	_timer.start()
 
-func _on_Timer_timeout():
-	_map.award_points()
-	time_remaining -= 1
-	_map.update_timer(time_remaining)
-
-func _process(delta):
-	# Called every frame. Delta is time since last frame.
-	# Update game logic here.
-	#if _map.has_winner(winning_score) or time_remaining <= 0:
-		#end_game()
+func _reset_match():
+	# for each player
+	for player in _players:
+		player.reset_score()
+	# set score to 0
+	# set time_remaining to time_limit
+	time_remaining = time_limit
+	# start timer again
+	_timer.start()
+	pass
+	
+func _show_scoreboard(score_board):
+	# set the scoreboard to visible
+	# ensure that the rankings are correct
 	pass
 
-#This is a work in progress... not completed
-func end_game():
+func _end_game():
 	var score_board = _map.collect_players()
+	# pause the timer
+	_timer.stop()
+	# show the score_board
+	_show_scoreboard(score_board)
+	# If "play again" button is hit or "start"
+	# reset the match
