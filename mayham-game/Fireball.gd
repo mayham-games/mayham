4ac49e7fc0a42cad08a4dd1bb640421d14c25aae
ezrajc <ezrajclark@gmail.com>
2019-03-15 19:53:48 -0600

    fireball works
    shouldnt line 25 work? Are the characters not kinimaticBodies?

--- mayham-game/Fireball.gd
+++ mayham-game/Fireball.gd
@@ -2,15 +2,19 @@
 extends Area2D
 
 const FIREBALL_SPEED = 300
+var _direction = 0
 
 func _ready():
 	set_process(true)
 	connect("body_entered", self, "on_body_entered")
+	
+func init(direction):
+	_direction = -1 * direction
 
 func _process(delta):
 	var speed_x = -1
 	var speed_y = 0
-	var motion = Vector2(speed_x, speed_y) * FIREBALL_SPEED
+	var motion = Vector2(speed_x * sign(_direction), speed_y) * FIREBALL_SPEED
 	position += motion * delta
 
 func _on_VisibilityNotifier2D_exit_screen():
