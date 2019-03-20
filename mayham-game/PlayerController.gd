f3c738141ff870d7d60a43a9c1e2c953d94e387a
mike0256 <michael.richards256@byu.edu>
2019-03-20 12:01:56 -0600

    Multiple controllers (#4)
    * Added input maps for multiple players
    
    Added new input maps for multiple players, updated the player controller script to only listen for the input from its specific player controller
    
    * Updating player controller based on merge
    
    Changed the variable player_num to be number after merge
    
    * Adding dynamic input event creation for controller inputs
    
    Added a dictionary structure that maps out the desired input settings to the desired action that the player controller should be looking for. Also added the function that traverses the dictionary and created/adds the input actions to the input map.
    Naming conventions follow the conventions that Godot uses.
    
    * Remapping action buttons
    
    Remapped what action is connected to which button, also added the special attack option to the player controller

--- mayham-game/PlayerController.gd
+++ mayham-game/PlayerController.gd
@@ -7,12 +7,65 @@ signal action_right
 signal action_dash
 signal action_stop
 signal action_attack
+signal action_special
+
+# Input Controls
+# Constants
+const ACTION_JUMP = "jump"
+const ACTION_ATTACK = "attack"
+const ACTION_SPECIAL = "special"
+const ACTION_DASH = "dash"
+const ACTION_LEFT = "left"
+const ACTION_RIGHT = "right"
+const ACTION_START = "start"
+
+const INPUT_TYPE = "type"
+const INPUT_BUTTON = "button"
+const INPUT_MOTION = "motion"
+
+const BUTTON_INDEX = "button_index"
+const MOTION_AXIS = "axis"
+const MOTION_AXIS_VALUE = "axis_value"
+
+const DEFAULT_CONTROLS = {
+	ACTION_JUMP : { 
+		INPUT_TYPE : INPUT_BUTTON,
+		BUTTON_INDEX : JOY_XBOX_A
+	},
+	ACTION_ATTACK : { 
+		INPUT_TYPE : INPUT_BUTTON,
+		BUTTON_INDEX : JOY_XBOX_B
+	},
+	ACTION_DASH : { 
+		INPUT_TYPE : INPUT_BUTTON,
+		BUTTON_INDEX : JOY_XBOX_X
+	},
+	ACTION_SPECIAL : { 
+		INPUT_TYPE : INPUT_BUTTON,
+		BUTTON_INDEX : JOY_XBOX_Y
+	},
+	ACTION_LEFT : { 
+		INPUT_TYPE : INPUT_MOTION,
+		MOTION_AXIS : JOY_AXIS_0,
+		MOTION_AXIS_VALUE : -1.0
+	},
+	ACTION_RIGHT : { 
+		INPUT_TYPE : INPUT_MOTION,
+		MOTION_AXIS : JOY_AXIS_0,
+		MOTION_AXIS_VALUE : 1.0
+	},
+	ACTION_START : {
+		INPUT_TYPE : INPUT_BUTTON,
+		BUTTON_INDEX : JOY_START
+	}
+}
 
 var playerDoll = null
 var number = 0
 
 func init(num):
 	number = num
+	_setupInputEvents(DEFAULT_CONTROLS)
 
 func _ready():
 	var position_x = 100 * (number - 5)
@@ -21,7 +74,38 @@ func _ready():
 	add_child(playerDoll)
 	playerDoll.init(number, position_x)
 	print("PC ready ", number)
-	
+
+func _setupInputEvents(controls):
+	for action in controls.keys():
+		
+		var control_input = controls[action]
+		
+		if control_input.has(INPUT_TYPE):
+			
+			# We only want to create the event if we know how to handle that type and we have all 
+			# the necessary information for its input type
+			if control_input[INPUT_TYPE] == INPUT_BUTTON and control_input.has(BUTTON_INDEX):
+				
+				var eventButton = InputEventJoypadButton.new()
+				eventButton.set_device(number - 1)
+				eventButton.set_button_index(control_input[BUTTON_INDEX])
+				eventButton.set_pressed(true)
+				
+				if not InputMap.has_action("p" + str(number) + "_" + action):
+					InputMap.add_action("p" + str(number) + "_" + action)
+				InputMap.action_add_event("p" + str(number) + "_" + action, eventButton)
+				
+			elif control_input[INPUT_TYPE] == INPUT_MOTION and control_input.has(MOTION_AXIS) and control_input.has(MOTION_AXIS_VALUE):
+				
+				var eventAxis = InputEventJoypadMotion.new()
+				eventAxis.set_device(number - 1)
+				eventAxis.set_axis(control_input[MOTION_AXIS])
+				eventAxis.set_axis_value(control_input[MOTION_AXIS_VALUE])
+				
+				if not InputMap.has_action("p" + str(number) + "_" + action):
+					InputMap.add_action("p" + str(number) + "_" + action)
+				InputMap.action_add_event("p" + str(number) + "_" + action, eventAxis)
+
 func _input(event):
 	if event.is_action_pressed("p" + str(number) + "_start"):
 		emit_signal("action_start")
@@ -37,6 +121,8 @@ func _input(event):
 		emit_signal("action_stop")
 	if event.is_action_pressed("p" + str(number) + "_attack"):
 		emit_signal("action_attack")
+	if event.is_action_pressed("p" + str(number) + "_special"):
+		emit_signal("action_special")
 
 func _process(delta):
 	if Input.is_action_pressed("p" + str(number) + "_left"):
