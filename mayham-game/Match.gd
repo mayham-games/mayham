94ece4d09e6302bb38004572f0ae68e978d27c25
SponJ2000 <43590538+SponJ2000@users.noreply.github.com>
2019-04-11 20:29:32 -0600

    Animation (#25)
    * Animation!
    
    * Animation!
    
    * Animation
    
    * Map tweak
    
    * Unique colors
    
    * Unique colors
    
    * Unique colors

--- mayham-game/Match.gd
+++ mayham-game/Match.gd
@@ -24,9 +24,10 @@ func _ready():
 	add_child(_map)
 	var playerController = ResourceLoader.load("res://PlayerController.tscn")
 	var players = []
+	var colors = makeColors(num_players)
 	for i in range(num_players):
 		var playerControl = playerController.instance()
-		playerControl.init(i+1)
+		playerControl.init(i+1, colors[i])
 		add_child(playerControl)
 		players.append(playerControl.get_player())
 	_map.init(players)
@@ -67,3 +68,32 @@ func _process(delta):
 #This is a work in progress... not completed
 func end_game():
 	var score_board = _map.collect_players()
+
+func makeColors(num_players):
+	var colors = []
+	for i in range(num_players):
+		var color = _random_color()
+		while not checkColor(color, colors):
+			color = _random_color()
+		colors.append(color)
+	return colors
+
+func checkColor(color, colorArray):
+	for i in range(colorArray.size()):
+		if color == colorArray[i]:
+			return false
+	return true
+
+func _random_color():
+	randomize()
+	var list = [1.0, 0.7, 0.0]
+	if randi()%2 == 0:
+		list = [1.0, 0.5, 0.2]
+	var x = randi()%list.size()
+	var c1 = list[x]
+	list.remove(x)
+	x = randi()%list.size()
+	var c2 = list[x]
+	list.remove(x)
+	var c3 = list[0]
+	return Color(c1,c2,c3)
