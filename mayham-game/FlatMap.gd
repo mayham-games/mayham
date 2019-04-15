43ffccd34b9b3c40ccdf9b0c48e3364f12c3f1d1
SponJ2000 <43590538+SponJ2000@users.noreply.github.com>
2019-04-15 08:18:52 -0600

    Map beautification 1 (#27)
    * announcer sound
    
    * More blocks, columns
    
    * New map, announcer
    
    * Added announcer and a logo

--- mayham-game/FlatMap.gd
+++ mayham-game/FlatMap.gd
@@ -1,8 +1,13 @@
 extends Node
 
 var goal = null
-onready var goal_positions = [ $GoalPosition1, $GoalPosition2, $GoalPosition3, $GoalPosition4 ]
+onready var goal_positions = [ $GoalPosition1, $GoalPosition2, $GoalPosition3, $GoalPosition4, $GoalPosition5 ]
 onready var timelabel = $MatchTime
+
+onready var mayham_particles = $ParticlesNode/MayhamParticles
+onready var mayham_timer = $ParticlesNode/Timer
+onready var announcer = $Announcer
+onready var s_mayham = ResourceLoader.load("res://sounds/a_mayham2.wav")
 
 func init(players):
 	goal = $Goal
@@ -11,6 +16,13 @@ func init(players):
 func _ready():
 	timelabel.add_color_override("font_color",Color(0,0,0,1))
 	timelabel.add_color_override("font_color_shadow",Color(1,1,1,1))
+	
+	mayham_timer.connect("timeout",self, "stop_particles")
+	mayham_particles.restart()
+	mayham_particles.emitting = true
+	mayham_timer.start()
+	announcer.stream = s_mayham
+	announcer.play(0)
 
 func move_goal():
 	var index = randi() % goal_positions.size()
@@ -29,3 +41,6 @@ func has_winner(winning_score):
 
 func update_timer(time):
 	$MatchTime.text = str(time)
+
+func stop_particles():
+	mayham_particles.emitting = false
